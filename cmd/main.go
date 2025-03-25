3a532f8fb6564699d0b18470cedb6a6375f6e851
Felipe Oliveira <felipe@funccloud.com>
2025-03-24 23:00:08 -0300

    add workspace api structure to fix e2e tests and back timeout to 5m
    Signed-off-by: Felipe Oliveira <felipe@funccloud.com>

--- cmd/main.go
+++ cmd/main.go
@@ -36,6 +36,9 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/metrics/filters"
 	metricsserver "sigs.k8s.io/controller-runtime/pkg/metrics/server"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
+
+	tenancyv1alpha1 "go.funccloud.dev/fcp/api/tenancy/v1alpha1"
+	tenancycontroller "go.funccloud.dev/fcp/internal/controller/tenancy"
 	// +kubebuilder:scaffold:imports
 )
 
@@ -47,6 +50,7 @@ var (
 func init() {
 	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
 
+	utilruntime.Must(tenancyv1alpha1.AddToScheme(scheme))
 	// +kubebuilder:scaffold:scheme
 }
 
@@ -198,6 +202,13 @@ func main() {
 		os.Exit(1)
 	}
 
+	if err = (&tenancycontroller.WorkspaceReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "Workspace")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	if metricsCertWatcher != nil {
