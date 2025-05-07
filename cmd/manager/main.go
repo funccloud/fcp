b03774e2b5634e00fc7a1191813524182ce0c241
Felipe Oliveira <felipe@funccloud.com>
2025-05-07 12:36:36 -0300

    use env as default value in manager

--- cmd/manager/main.go
+++ cmd/manager/main.go
@@ -57,7 +57,7 @@ func main() {
 	var enableHTTP2 bool
 	var domain string
 	var tlsOpts []func(*tls.Config)
-	flag.StringVar(&domain, "domain", "fcp.funccloud.dev", "The domain name used for the FCP Platform.")
+	flag.StringVar(&domain, "domain", os.Getenv("DEFAULT_DOMAIN"), "The domain name used for the FCP Platform.")
 	flag.StringVar(&metricsAddr, "metrics-bind-address", "0", "The address the metrics endpoint binds to. "+
 		"Use :8443 for HTTPS or :8080 for HTTP, or leave as 0 to disable the metrics service.")
 	flag.StringVar(&probeAddr, "health-probe-bind-address", ":8081", "The address the probe endpoint binds to.")
