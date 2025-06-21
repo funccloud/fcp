1245472091ec8af5b19456125c947b31fdc3c070
Felipe Oliveira <felipe@funccloud.com>
2025-06-21 17:52:44 -0300

    ui config for k8s
    Signed-off-by: Felipe Oliveira <felipe@funccloud.com>

--- ui/src/app/app.routes.ts
+++ ui/src/app/app.routes.ts
@@ -1,4 +1,7 @@
 import { Routes } from '@angular/router';
 import { NotFound } from './shared/not-found/not-found';
 
-export const routes: Routes = [{ path: '**', component: NotFound }];
+export const routes: Routes = [
+  { path: '', component: NotFound },
+  { path: '**', component: NotFound },
+];
--- ui/src/server.ts
+++ ui/src/server.ts
@@ -24,6 +24,10 @@ const angularApp = new AngularNodeAppEngine();
  * ```
  */
 
+app.get('/healthz', (req, res) => {
+  res.json({ status: 'OK' });
+});
+
 /**
  * Serve static files from /browser
  */
@@ -32,7 +36,7 @@ app.use(
     maxAge: '1y',
     index: false,
     redirect: false,
-  }),
+  })
 );
 
 /**
@@ -42,7 +46,7 @@ app.use((req, res, next) => {
   angularApp
     .handle(req)
     .then((response) =>
-      response ? writeResponseToNodeResponse(response, res) : next(),
+      response ? writeResponseToNodeResponse(response, res) : next()
     )
     .catch(next);
 });
