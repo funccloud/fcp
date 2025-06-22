1bb714a1aa16d7c7b984f1f8e9ac689554a7dc73
Felipe Oliveira <felipe@funccloud.com>
2025-06-21 22:16:02 -0300

    fix router
    Signed-off-by: Felipe Oliveira <felipe@funccloud.com>

--- ui/src/app/app.config.ts
+++ ui/src/app/app.config.ts
@@ -6,12 +6,16 @@ import {
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import {
+  provideClientHydration,
+  withEventReplay,
+} from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes), provideClientHydration(withEventReplay()),
+    provideClientHydration(withEventReplay()),
+    provideRouter(routes),
   ],
 };
