ec88eb91bbd36faa7c2c6bfb12fef1185a66c62a
Felipe Oliveira <felipe@funccloud.com>
2025-06-21 23:35:02 -0300

    fmt
    Signed-off-by: Felipe Oliveira <felipe@funccloud.com>

--- ui/src/app/app.routes.server.ts
+++ ui/src/app/app.routes.server.ts
@@ -2,17 +2,17 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 import { routes } from './app.routes';
 
 const serverRoutes: ServerRoute[] = routes
-  .filter(route => route.path !== '**')
-  .map(route => ({
+  .filter((route) => route.path !== '**')
+  .map((route) => ({
     path: route.path!,
     renderMode: RenderMode.Server,
-    status: 200
+    status: 200,
   }));
 
 serverRoutes.push({
   path: '**',
   renderMode: RenderMode.Server,
-  status: 404
+  status: 404,
 });
 
 export { serverRoutes };
