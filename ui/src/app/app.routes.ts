066f53991e13beb81b678f08b0aac9c66b25e09e
Bruno Souza <brunosouzadev@gmail.com>
2025-06-24 20:20:41 -0300

    check point componentes

--- ui/src/app/app.routes.ts
+++ ui/src/app/app.routes.ts
@@ -1,7 +1,14 @@
 import { Routes } from '@angular/router';
 import { NotFound } from './shared/not-found/not-found';
+import { ApplicationModule } from '@angular/core';
+import { Applications } from './private/applications/applications';
 
 export const routes: Routes = [
+
+  {
+    path: 'application',
+    component: Applications,
+  },
   {
     path: '**',
     component: NotFound,
