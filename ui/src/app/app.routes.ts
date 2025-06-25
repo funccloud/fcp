a9568ca76c6d6e0404c752a728cf7fdf16e333fb
Felipe Oliveira <felipe@funccloud.com>
2025-06-24 22:22:04 -0300

    just tailwind and angular material integration
    Signed-off-by: Felipe Oliveira <felipe@funccloud.com>

--- ui/src/app/app.routes.ts
+++ ui/src/app/app.routes.ts
@@ -1,14 +1,7 @@
 import { Routes } from '@angular/router';
 import { NotFound } from './shared/not-found/not-found';
-import { ApplicationModule } from '@angular/core';
-import { Applications } from './private/applications/applications';
 
 export const routes: Routes = [
-
-  {
-    path: 'application',
-    component: Applications,
-  },
   {
     path: '**',
     component: NotFound,
